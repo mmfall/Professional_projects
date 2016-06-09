4e7369cde9377b2675c34687e03d5c1cc843f7e1
Mame-Mor <mamemor2@gmail.com>
2016-06-09 19:10:40 +0200

    Add files via upload

---
+++ R2D2.py
@@ -0,0 +1,108 @@
+Import aiml, yampy, os
+
+#Authenticate to Yammer
+
+authenticator = yampy.Authenticator(client_id=MY_CLIENT_ID,client_secret=MY_CLIENT_SECRET)
+yammer = yampy.Yammer(access_token=access_token)
+
+# Initiate variable new message to constantly check for new private messages
+lastmessageid=0
+
+# Create the kernel and learn AIML files
+kernel=aiml.Kernel()
+
+if os.path.isfile("AFR2D2.brn"):
+kernel.bootstrap(brainFile = "AFR2D2.brn")
+
+else:
+
+kernel.learn("reduction.names.aiml")
+kernel.learn("reduction0.safe.aiml")
+kernel.learn("reduction1.safe.aiml")
+kernel.learn("reduction2.safe.aiml")
+kernel.learn("reduction3.safe.aiml")
+kernel.learn("reduction4.safe.aiml")
+kernel.learn("reductions-update.aiml")
+
+kernel.learn("mp0.aiml")
+kernel.learn("mp1.aiml")
+kernel.learn("mp2.aiml")
+kernel.learn("mp3.aiml")
+kernel.learn("mp4.aiml")
+kernel.learn("mp5.aiml")
+kernel.learn("mp6.aiml")
+
+
+kernel.learn("ai.aiml")
+kernel.learn("alice.aiml")
+kernel.learn("astrology.aiml")
+kernel.learn("atomic.aiml")
+kernel.learn("badanswer.aiml")
+kernel.learn("biography.aiml")
+kernel.learn("bot.aiml")
+kernel.learn("bot_profile.aiml")
+kernel.learn("client.aiml")
+kernel.learn("client_profile.aiml")
+kernel.learn("computers.aiml")
+kernel.learn("continuation.aiml")
+kernel.learn("date.aiml")
+kernel.learn("default.aiml")
+kernel.learn("drugs.aiml")
+kernel.learn("emotion.aiml")
+kernel.learn("food.aiml")
+kernel.learn("geography.aiml")
+kernel.learn("gossip.aiml")
+kernel.learn("history.aiml")
+kernel.learn("humor.aiml")
+kernel.learn("imponderables.aiml")
+kernel.learn("inquiry.aiml")
+kernel.learn("interjection.aiml")
+kernel.learn("iu.aiml")
+kernel.learn("junktest.text")
+kernel.learn("knowledge.aiml")
+kernel.learn("literature.aiml")
+kernel.learn("loebner10.aiml")
+kernel.learn("money.aiml")
+kernel.learn("movies.aiml")
+kernel.learn("music.aiml")
+kernel.learn("numbers.aiml")
+kernel.learn("personality.aiml")
+kernel.learn("phone.aiml")
+kernel.learn("pickup.aiml")
+kernel.learn("politics.aiml")
+kernel.learn("primeminister.aiml")
+kernel.learn("primitive-math.aiml")
+kernel.learn("psychology.aiml")
+kernel.learn("religion.aiml")
+kernel.learn("salutations.aiml")
+kernel.learn("science.aiml")
+kernel.learn("sex.aiml")
+kernel.learn("sports.aiml")
+kernel.learn("stack.aiml")
+kernel.learn("stories.aiml")
+kernel.learn("that.aiml")
+kernel.learn("wallace.aiml")
+kernel.learn("xfind.aiml")
+kernel.learn("update_mccormick.aiml")
+
+kernel.learn("update1.aiml")
+
+kernel.saveBrain("AFR2D2.brn")
+
+
+
+while True:
+
+ if yammer.messages.private(limit=1).message_id!=lastmessage
+  lastmessageuser=yammer.messages.private(limit=1).user_id  
+  lastmessagecontent=yammer.messages.private(limit=1).message_content
+  lastmessageid=yammer.messages.private(limit=1).message_id
+  
+  
+  lastresponse=kernel.respond(lastmessagecontent)
+
+
+  yammer.messages.create(lastresponse,replied_to_id=lastmessageid)
+
+
+
